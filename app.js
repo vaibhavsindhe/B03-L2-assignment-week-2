d6568841747e05e39b0d4cda8d5dcced91b91ff6
vaibhav <vaibhavsindhe10@email.com>
2024-03-20 14:34:41 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
     const nav = [
         {
-            logo: "logo.png",
+            logo:"Logo.png",
             links: {
                 shop: "#",
                 brand: "#",
